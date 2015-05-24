a90c0491542dfb441e23136df0c248262adc8503
Thundercat <michael.c.haines@gmail.com>
2015-05-23 18:09:27 -0600

    Site skeleton and homepage mostly done

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+var app = angular.module("app", ["ngRoute"]);
+
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'templates/homepage.html'
+  })
+  .when('/services', {
+    templateUrl: 'templates/services.html'
+  })
+  .when('/about', {
+    templateUrl: 'templates/about.html'
+  })
+  .when('/contact', {
+    templateUrl: 'templates/contact.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
+}]);
+
+
+app.controller('appCtrl', function($scope, $location){
+	$scope.isActive = function(route) {
+        return route === $location.path();
+    }
+	
+})
